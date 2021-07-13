f39e2dd0a8156ed86ccdd4348f08201830cd335f
silicon <dingyoulon@gmail.com>
2021-07-14 00:30:48 +0800

    remove

---
+++ build/CMakeFiles/RS.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/root/research/RS/Base.cpp" "CMakeFiles/RS.dir/Base.cpp.o" "gcc" "CMakeFiles/RS.dir/Base.cpp.o.d"
+  "/root/research/RS/Builder.cpp" "CMakeFiles/RS.dir/Builder.cpp.o" "gcc" "CMakeFiles/RS.dir/Builder.cpp.o.d"
+  "/root/research/RS/Client.cpp" "CMakeFiles/RS.dir/Client.cpp.o" "gcc" "CMakeFiles/RS.dir/Client.cpp.o.d"
+  "/root/research/RS/Data.cpp" "CMakeFiles/RS.dir/Data.cpp.o" "gcc" "CMakeFiles/RS.dir/Data.cpp.o.d"
+  "/root/research/RS/Server.cpp" "CMakeFiles/RS.dir/Server.cpp.o" "gcc" "CMakeFiles/RS.dir/Server.cpp.o.d"
+  "/root/research/RS/main.cpp" "CMakeFiles/RS.dir/main.cpp.o" "gcc" "CMakeFiles/RS.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
